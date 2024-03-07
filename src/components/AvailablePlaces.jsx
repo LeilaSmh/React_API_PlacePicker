fbc42601aa265a94c4e0d0f063447c913eb98623
LeilaSmh <leilasamih.ls@gmail.com>
2024-03-07 16:00:35 +0100

    handling HTTP errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,39 @@
 import { useState } from "react";
 import Places from "./Places.jsx";
 import { useEffect } from "react";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    setIsLoading(true);
     const fetchPlaces = async () => {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later!",
+        });
+      }
       setIsLoading(false);
     };
     fetchPlaces();
   }, []);
+
+  if (error)
+    return <Error title="An error occurred!" message={error.message} />;
 
   return (
     <Places
